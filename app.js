fea522d25975960880a50baec78c3d1dcdb3bded
Christian Fürst <c.fuerst.home@gmail.com>
2021-03-01 20:54:06 +0100

    added burn function

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const _ = require("lodash");
 const faker = require("faker");
-const { Api, JsonRpc, RpcError } = require("eosjs");
+const { Api, JsonRpc } = require("eosjs");
 const { JsSignatureProvider } = require("eosjs/dist/eosjs-jssig");
 const fetch = require("node-fetch");
 const { TextEncoder, TextDecoder } = require("util");
@@ -18,91 +18,177 @@ const api = new Api({
   textEncoder: new TextEncoder(),
 });
 
-let timestamp = Math.round(new Date() / 1000);
+function fetchNftArray() {
+  return new Promise((resolve, reject) => {
+    axios
+      .post("https://wax.cryptolions.io/v1/chain/get_table_rows", {
+        json: true,
+        code: "simpleassets",
+        scope: config.account,
+        table: "sassets",
+        table_key: "author",
+        lower_bound: "ilovekolobok",
+        upper_bound: "ilovekoloboka",
+        index_position: 2,
+        key_type: "i64",
+        limit: 10000,
+        reverse: false,
+        show_payer: false,
+      })
+      .then((result) => {
+        let nftArray = [];
 
-let nftOne;
-let nftTwo;
+        result.data.rows.forEach((element) => {
+          let idata = JSON.parse(element.idata);
+          let mdata = JSON.parse(element.mdata);
 
-let nftArray = [];
-let nftArrayCandidates = [];
+          nftArray.push({
+            id: element.id,
+            gen: idata.gen,
+            genome: idata.genome,
+            idata: idata,
+            mdata: mdata,
+          });
+        });
 
-axios
-  .post("https://wax.cryptolions.io/v1/chain/get_table_rows", {
-    json: true,
-    code: "simpleassets",
-    scope: config.account,
-    table: "sassets",
-    table_key: "author",
-    lower_bound: "ilovekolobok",
-    upper_bound: "ilovekoloboka",
-    index_position: 2,
-    key_type: "i64",
-    limit: 1000,
-    reverse: false,
-    show_payer: false,
-  })
-  .then((result) => {
-    result.data.rows.forEach((element) => {
-      let idata = JSON.parse(element.idata);
-      let mdata = JSON.parse(element.mdata);
+        resolve(nftArray);
 
-      nftArray.push({
-        id: element.id,
-        gen: idata.gen,
-        genome: idata.genome,
-        idata: idata,
-        mdata: mdata,
+        console.log("fetchNftArray success.");
+      })
+      .catch((error) => {
+        reject(error);
       });
-    });
-
-    nftArrayCandidates = _.orderBy(
-      nftArray,
-      ["idata.gen", "mdata.cd"],
-      ["desc", "asc"]
-    );
-
-    nftArrayCandidates = _.filter(nftArrayCandidates, {
-      mdata: { health: 100 },
-    });
-
-    nftArrayCandidates = _.filter(nftArrayCandidates, function (o) {
-      return o.mdata.cd < timestamp;
-    });
-
-    nftOne = nftArrayCandidates[0];
-    nftTwo = nftArrayCandidates[1];
-
-    if ((nftOne.mdata.cd < timestamp) & (nftTwo.mdata.cd < timestamp)) {
-      console.log("We've got a match!");
-
-      (async () => {
-        const result = await api.transact(
-          {
-            actions: [
-              {
-                account: "ilovekolobok",
-                name: "breed",
-                authorization: [
-                  {
-                    actor: config.account,
-                    permission: "active",
-                  },
-                ],
-                data: {
-                  owner: config.account,
-                  name: faker.name.findName(),
-                  parent1: nftOne.id,
-                  parent2: nftTwo.id,
-                },
-              },
-            ],
-          },
-          {
-            blocksBehind: 3,
-            expireSeconds: 30,
-          }
-        );
-        console.dir(result);
-      })();
-    }
   });
+}
+
+function burnNft(nftArray) {
+  let nftArrayCandidates = [];
+
+  // Filtern auf 100 health
+  nftArrayCandidates = _.filter(nftArray, {
+    mdata: { health: -1 },
+  });
+
+  if (nftArrayCandidates.length > 0) {
+    let idsArray = [];
+
+    nftArrayCandidates.forEach((element) => {
+      idsArray.push(element.id);
+    });
+
+    console.log("Found NFTs to burn! Broadcasting transaction...");
+
+    api
+      .transact(
+        {
+          actions: [
+            {
+              account: "simpleassets",
+              name: "burn",
+              authorization: [
+                {
+                  actor: config.account,
+                  permission: "active",
+                },
+              ],
+              data: {
+                owner: config.account,
+                assetids: idsArray,
+              },
+            },
+          ],
+        },
+        {
+          blocksBehind: 3,
+          expireSeconds: 30,
+        }
+      )
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  } else {
+    console.log("No NFT to burn. Waiting for next iteration...");
+  }
+}
+
+function matchPairs(nftArray) {
+  let timestamp = Math.round(new Date() / 1000);
+
+  let nftOne;
+  let nftTwo;
+
+  let nftArrayCandidates = [];
+
+  // Nach gen absteigend sortieren
+  nftArrayCandidates = _.orderBy(
+    nftArray,
+    ["idata.gen", "mdata.cd"],
+    ["desc", "asc"]
+  );
+
+  // Filtern auf 100 health
+  nftArrayCandidates = _.filter(nftArrayCandidates, {
+    mdata: { health: 100 },
+  });
+
+  // Filtern auf alle ohne cd
+  nftArrayCandidates = _.filter(nftArrayCandidates, function (o) {
+    return o.mdata.cd < timestamp;
+  });
+
+  nftOne = nftArrayCandidates[0];
+  nftTwo = nftArrayCandidates[1];
+
+  if ((nftOne.mdata.cd < timestamp) & (nftTwo.mdata.cd < timestamp)) {
+    console.log("We've got a match! Broadcasting transaction...");
+
+    api
+      .transact(
+        {
+          actions: [
+            {
+              account: "ilovekolobok",
+              name: "breed",
+              authorization: [
+                {
+                  actor: config.account,
+                  permission: "active",
+                },
+              ],
+              data: {
+                owner: config.account,
+                name: faker.name.findName(),
+                parent1: nftOne.id,
+                parent2: nftTwo.id,
+              },
+            },
+          ],
+        },
+        {
+          blocksBehind: 3,
+          expireSeconds: 30,
+        }
+      )
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  } else {
+    console.log("No match available. Waiting for next iteration...");
+  }
+}
+
+function loop() {
+  fetchNftArray().then((nftArray) => {
+    matchPairs(nftArray);
+    burnNft(nftArray);
+  });
+}
+
+loop();
+setInterval(loop, 60000);
